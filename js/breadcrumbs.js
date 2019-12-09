6eb6784fee0c93dba0c40c33107f1581156dd583
Marco Nie <github@niekun.net>
2019-12-09 14:30:31 +0800

    init build

---
+++ js/breadcrumbs.js
@@ -0,0 +1,9 @@
+/*!
+ *
+ * This file is part of the nginx-fancyindex-flat-theme, licensed under the GNU
+ * General Public License. See the LICENSE file for details.
+ *
+ * Copyright (C)
+ *  2018 Alexander Haase <ahaase@alexhaase.de>
+ */
+function generateBreadcrumbs(){function e(e,n,r){return'<li class="breadcrumb-item'+(r?' active aria-current="page':"")+'">'+(r?"":'<a href="'+n+'">')+e+(r?"":"</a>")}for(var n=window.location.pathname.replace(/\/$/,"").split("/"),r="",a="",t=0;t<n.length;t++)a+=n[t]+"/",r+=e(0==t?"Home":decodeURIComponent(n[t]),a,t==n.length-1);document.getElementById("breadcrumbs").innerHTML=r}

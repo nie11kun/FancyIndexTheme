776e81faab7feebf1843309bb8a06fadc0d41d7f
Marco Nie <github@niekun.net>
2019-12-12 16:02:35 +0800

    sort js format

--- js/breadcrumbs.js
+++ js/breadcrumbs.js
@@ -3,14 +3,14 @@
  * This file is part of the nginx-fancyindex-flat-theme, licensed under the GNU
  */
 function generateBreadcrumbs() {
-    function navBar(e, n, r) {
-        return '<li class="breadcrumb-item' + (r ? ' active aria-current="page' : "") + '">' + (r ? "" : '<a href="' + n + '">') + e + (r ? "" : "</a>");
+    function navBar(folderName, folderURL, lastFolder) {
+        return '<li class="breadcrumb-item' + (lastFolder ? ' active aria-current="page' : "") + '">' + (lastFolder ? "" : '<a href="' + folderURL + '">') + folderName + (lastFolder ? "" : "</a>");
     }
 
-    for (var n = window.location.pathname.replace(/\/$/, "").split("/"), r = "", a = "", t = 0; t < n.length; t++) {
-        a += n[t] + "/";
-        r += navBar(0 == t ? "Home" : decodeURIComponent(n[t]), a, t == n.length - 1);
+    for (var folders = window.location.pathname.replace(/\/$/, "").split("/"), listBar = "", folderURL = "", i = 0; i < folders.length; i++) {
+        folderURL += folders[i] + "/";
+        listBar += navBar(i == 0 ? "Home" : decodeURIComponent(folders[i]), folderURL, i == folders.length - 1);
     }
     
-    document.getElementById("breadcrumbs").innerHTML = r;
+    document.getElementById("breadcrumbs").innerHTML = listBar;
 }

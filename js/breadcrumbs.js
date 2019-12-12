9b85943833646379222422e31a973b63df0ce39d
Marco Nie <github@niekun.net>
2019-12-12 21:19:03 +0800

    test

--- js/breadcrumbs.js
+++ js/breadcrumbs.js
@@ -4,13 +4,13 @@
  */
 function generateBreadcrumbs() {
     function navBar(folderName, folderURL, lastFolder) {
-        return '<li class="breadcrumb-item' + (lastFolder ? ' active aria-current="page' : "") + '">' + (lastFolder ? "" : '<a href="' + folderURL + '">') + folderName + (lastFolder ? "" : "</a>");
+        return '<li class="breadcrumb-item' + (lastFolder ? ' active" aria-current="page"' : '"') + '>' + (lastFolder ? "" : '<a href="' + folderURL + '">') + folderName + (lastFolder ? "" : "</a>");
     }
 
     for (var folders = window.location.pathname.replace(/\/$/, "").split("/"), listBar = "", folderURL = "", i = 0; i < folders.length; i++) {
         folderURL += folders[i] + "/";
-        listBar += navBar(i == 0 ? '<i class="fa fa-fw fa-home" aria-hidden="true"></i> Home' : decodeURIComponent(folders[i]), folderURL, i == folders.length - 1);
+        listBar += navBar(i == 0 ? 'Home' : decodeURIComponent(folders[i]), folderURL, i == folders.length - 1);
     }
-    
+
     document.getElementById("breadcrumbs").innerHTML = listBar;
 }
